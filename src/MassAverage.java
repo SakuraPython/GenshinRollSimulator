6a22e5710bacdd456a77422813f61704a6108eb9
SakuraPython <godofpythons@gmail.com>
2021-06-28 20:47:16 -0400

    Add in best and worst cases

--- src/MassAverage.java
+++ src/MassAverage.java
@@ -3,6 +3,8 @@ public class MassAverage {
     static double averagePrimos = 0;
     static double averageCost = 0;
     static double averageRolls = 0;
+    static int bestRolls = 9999999;
+    static int worstRolls = 0;
     static final int timesToRun = 1000000;
     static final int constellation = 6;
     static final int refinement = 5;
@@ -29,6 +31,8 @@ public class MassAverage {
         System.out.println("OLD SYSTEM");
         System.out.println("You rolled an average of " + averageRolls + " times.");
         System.out.println("The average primos it took was " + averagePrimos + " and this would cost $" + averageCost + " for the old system.");
+        System.out.println("Best rolls: " + bestRolls + " (= " + bestRolls * 160 + " primos or $" + (bestRolls * 160) / 80.8 + ')');
+        System.out.println("Worst rolls: " + worstRolls + " (= " + worstRolls * 160 + " primos or $" + (worstRolls * 160) / 80.8 + ')');
         size = 1;
         simulator.reset(constellation, refinement);
         simulator.RollNewSystem();
@@ -42,6 +46,8 @@ public class MassAverage {
         System.out.println("NEW SYSTEM");
         System.out.println("You rolled an average of " + averageRolls + " times.");
         System.out.println("The average primos it took was " + averagePrimos + " and this would cost $" + averageCost + " for the new system.");
+        System.out.println("Best rolls: " + bestRolls + " (= " + bestRolls * 160 + " primos or $" + (bestRolls * 160) / 80.8 + ')');
+        System.out.println("Worst rolls: " + worstRolls + " (= " + worstRolls * 160 + " primos or $" + (worstRolls * 160) / 80.8 + ')');
         simulator.reset(constellation, refinement);
         i = 0;
         size = 1;
@@ -55,6 +61,8 @@ public class MassAverage {
         System.out.println("WEAPON SYSTEM");
         System.out.println("You rolled an average of " + averageRolls + " times.");
         System.out.println("The average primos it took was " + averagePrimos + " and this would cost $" + averageCost + " for the weapon system.");
+        System.out.println("Best rolls: " + bestRolls + " (= " + bestRolls * 160 + " primos or $" + (bestRolls * 160) / 80.8 + ')');
+        System.out.println("Worst rolls: " + worstRolls + " (= " + worstRolls * 160 + " primos or $" + (worstRolls * 160) / 80.8 + ')');
     }
 
     private static int run(RollSim simulator, int i) {
@@ -62,6 +70,12 @@ public class MassAverage {
         averagePrimos = (averagePrimos * (size-1) + simulator.calculatePrimos()) / size;
         averageCost = (averageCost * (size-1) + simulator.calculateRealDollars()) / size;
         averageRolls = (averageRolls * (size-1) + simulator.getRolls()) / size;
+        if (simulator.getRolls() < bestRolls) {
+            bestRolls = simulator.getRolls();
+        }
+        if (simulator.getRolls() > worstRolls) {
+            worstRolls = simulator.getRolls();
+        }
         simulator.reset(constellation, refinement);
         i++;
         return i;
